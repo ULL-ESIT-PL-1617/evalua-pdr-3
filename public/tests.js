f721700c973babe3a6271de5c453b7160f689987
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-04-19 21:00:03 +0100

    public/tests.js

--- public/tests.js
+++ public/tests.js
@@ -6,14 +6,14 @@
   suite('parser', function() {
     setup(function() {
     });
-    test('Numbers are parsed correctly', () => {
-      var original = parse('4');
-      assert.deepEqual(original, 4, "Son iguales!");
+    test('Commas are parsed correctly', () => {
+      var original = parse('4+2, 3');
+      assert.deepEqual(original, { result: 3, results: [ 6, 3 ] }, "OK!");
     });
     test('Multiplications are parsed correctly', () => {
-      var result = parse('4*2');
+      var result = parse('4*2, 2-1-1');
       console.log(result);
-      assert.deepEqual(result, 8,"Son iguales!");
+      assert.deepEqual(result, { result: 0, results: [ 8, 0 ] } ,"OK!");
     });
     test('Bad expressions throw exceptions', () => {
       assert.throws(() => parse('3 + (4+2))'), /Syntax\s+Error/i);
